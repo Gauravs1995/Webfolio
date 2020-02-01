001e671c1bace49bc4c8e2aaac65ef0c21890e19
Gauravs1995 <48901761+Gauravs1995@users.noreply.github.com>
2020-02-02 00:20:24 +1000

    initial commit - header, landing-page, side-drawer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SideDrawerComponent } from './side-drawer/side-drawer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingPageComponent,
+    SideDrawerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/side-drawer/side-drawer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-side-drawer",
+  templateUrl: "./side-drawer.component.html",
+  styleUrls: ["../header/header.component.css", "./side-drawer.component.css"]
+})
+export class SideDrawerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
